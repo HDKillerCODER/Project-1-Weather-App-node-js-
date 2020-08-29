a5759d973df6bec660daa33664d2a8a05c8948c7
Harsh <harshdhiman9484@gmail.com>
2020-08-29 15:27:38 +0530

    Modified app.js file

--- src/app.js
+++ src/app.js
@@ -115,7 +115,7 @@ const forecast = require('./utils/forecast.js')
 
 const app = express()
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 // Defining paths (express config)
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewspath = path.join(__dirname, '../templates/views');
